ffe0c0a6767e27e95debd374082feab015b6ea17
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2022-06-14 07:54:41 +0100

    updated

---
+++ get_data.py
@@ -0,0 +1,26 @@
+
+
+# ---
+
+
+Download via dropbox by going to this link:
+https://www.dropbox.com/s/p8pwo053nmemiqr/data.tar.gz?dl=0
+
+Extract the archive to data/ in the same directory.
+
+
+
+
+
+# --- This doesn't work yet. For EB to figure out.
+
+# import requests
+# URL = 'https://uc17a2e9a547aa9b3f3188d4a8cf.dl.dropboxusercontent.com/cd/0/get/BnLCUTwdnJA03DMMUgy8rd4w2al-DhoKBkEXZNQdNKiAPsh6rxS5O8JU4LBooZ_DnFgSaOB1T5-ChsTZf4zzXhsVEkmIzWhdvDAv6RdeW79Do8C6Q3Pw0DUDuaiws7be0NAlXuHVlJUncCRAcoL61wexWdBVnyQxRwGdlaMCVlQfSQ/file#'
+#
+# response = requests.get(URL)
+# open('data1.tar.gz', 'wb').write(response.content)
+#
+# import tarfile
+# tar = tarfile.open('data1.tar.gz')
+# tar.extractall()
+# tar.close()
